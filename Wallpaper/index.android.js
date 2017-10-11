688f9f325d0f189ce08f81a07f7e5c3efa89e9d8
John Barczynski <jeb5875@psu.edu>
2017-10-11 17:37:57 -0400

    Working on Android, too - thank god

--- Wallpaper/index.android.js
+++ Wallpaper/index.android.js
@@ -9,45 +9,99 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  Dimensions,
 } from 'react-native';
+import Swiper from 'react-native-swiper';
 
-export default class Wallpaper extends Component {
+let {width, height} = Dimensions.get('window'); //gets screen dimensions
+
+export default class Wallpaper extends Component { //class
+  constructor(props) { //constructor
+    super(props); //properties of class
+    this.state = { //state
+      images: [], //all images
+      isLoading: true, //loading status
+    };
+  }
+
+  //Invoked when component is mounted
+  componentDidMount() {
+    this.fetchWallpapers();
+  }
+
+  //Renders physical elements onto screen
   render() {
+    if(!this.state.isLoading) { //checks if done loading
+      return this.renderResults(); //if done, load results
+    } else {
+      return this.renderLoadingView(); //if not done, show loading screen
+    }
+  }
+
+  renderLoadingView() { //loading view
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+      <View style={styles.loading}>
+        <Text>Loading...</Text>
       </View>
     );
   }
+
+  renderResults() { //results view
+    let images = this.state.images;
+    return (
+      <Swiper>
+        {
+          images.map( (image, index) => {
+            return (
+              <View key={index}>
+                <Image
+                  source={{uri: `https://unsplash.it/${image.width}/${image.height}?image=${image.id}`}}
+                  style={styles.wallpaperImage}
+                />
+              </View>
+            );
+          })
+        }
+      </Swiper>
+    );
+
+  }
+
+  fetchWallpapers() { //api call
+    let url = 'https://unsplash.it/list';
+    fetch(url)
+      .then( response => response.json())
+      .then( jsonData => {
+        let tempImages = [];
+        for(i = 0; i < 100; i+=20) {
+          tempImages.push(jsonData[i]);
+        }
+        console.log(tempImages);
+        this.setState({
+          images: [].concat(tempImages),
+          isLoading: false,
+        });
+
+      })
+      .catch( error => { console.log(error); });
+  }
 }
 
+// Style sheet
 const styles = StyleSheet.create({
-  container: {
+  loading: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: 'center',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  wallpaperImage: {
+    flex: 1,
+    width: width,
+    height: height,
+    backgroundColor: '#000',
+  }
 });
 
 AppRegistry.registerComponent('Wallpaper', () => Wallpaper);
